5e24e2f96ba2c37acda27e40799330d1c877339d
Areahints <areahints@gmail.com>
2019-05-18 12:29:23 +0100

    First Push

---
+++ engine/log.py
@@ -0,0 +1,15 @@
+# author: Nwaezuoke Kenechukwu
+# MIT License
+
+# --------------------------
+# Logs
+# --------------------------
+
+import logging
+
+def set_custom_log_info(filename):
+    logging.basicConfig(filename=filename, level=logging.INFO)
+    
+def report(e:Exception):
+    logging.exception(str(e))
+
---
+++ start.py
@@ -0,0 +1,32 @@
+# author: 
+# MIT License
+
+# --------------------------
+#  PunchNG.com Demo
+# --------------------------
+
+## News title, link scraping
+
+from engine import log
+from engine import punch
+from engine import engine
+
+if __name__ == '__main__':
+    # Define log file location
+    log.set_custom_log_info('logs/error.log')
+
+    # SSL or HTTPS ISSUE
+    engine.verify_https_issue()
+
+    # create scraping object
+    punch_scrap = punch.Punch(punch.url_punch, log)
+
+    # checking if we should redownload from url or not
+    if engine.check_cache(punch.raw_html, punch.CACHE):
+        punch_scrap.retrieve_webpage()
+        punch_scrap.write_webpage_as_html()
+
+    punch_scrap.read_webpage_from_html()
+    punch_scrap.convert_data_to_bs4()
+    #punch_scrap.print_beautiful_soup()
+    punch_scrap.parse_soup_to_simple_html()
